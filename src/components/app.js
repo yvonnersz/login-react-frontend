8dbee22bad57bb624e010098458d8f98f6c40a8c
Yvonne <yvonnersz@gmail.com>
2020-12-22 23:43:59 -0800

    build feature so user stays logged in

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Home from './Home';
 import Dashboard from './Dashboard';
+import axios from 'axios';
 
 export default class App extends Component {
   constructor() {
@@ -12,6 +13,28 @@ export default class App extends Component {
     };
 
     this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  checkLoginStatus() {
+    axios.get('http://localhost:3001/logged_in', {withCredentials: true}).then(response => {
+      if (response.data.logged_in && this.state.loggedInStatus === "NOT_LOGGED_IN") {
+        this.setState({
+          loggedInStatus: "LOGGED_IN",
+          user: response.data.user
+        })
+      } else if (!response.data.logged_in && this.state.loggedInStatus === "LOGGED_IN") {
+        this.setState({
+          loggedInStatus: "NOT_LOGGED_IN",
+          user: {}
+        })
+      }
+    }).catch(error => {
+      console.log(error)
+    })
+  }
+
+  componentDidMount() {
+    this.checkLoginStatus();
   }
 
   handleLogin(data) {
